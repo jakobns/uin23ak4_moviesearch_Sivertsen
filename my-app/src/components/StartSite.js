88a85477dbb39127f22830893ebe618879bd6f03
jakobns <jakoblksiv@gmail.com>
2023-03-21 19:48:21 +0100

    javascript update

--- my-app/src/components/StartSite.js
+++ my-app/src/components/StartSite.js
@@ -5,7 +5,7 @@ function StartSite() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=a2a5d73d&s=James+Bond')
+    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=a2a5d73d&s=James+Bond&type=movie')
       .then(response => response.json())
       .then(data => setMovies(data.Search));
   }, []);
